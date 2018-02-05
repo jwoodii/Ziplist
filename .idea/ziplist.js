d3fde4dd15c9814829795b3f8ffbc2fe8356c3b9
jwoodii <35505841+jwoodii@users.noreply.github.com>
2018-02-05 09:44:09 -1000

    WOD
    Time 15 minutes 26 seconds

---
+++ .idea/ziplist.js
@@ -0,0 +1,19 @@
+function zipList(array1, array2) {
+  let array = [];
+  let i = 0;
+  for (i = 0; i < array1.length; i++) {
+    array.push(array1[i]);
+    array.push(array2[i]);
+  }
+  return array;
+}
+
+function zipListTheSimpleWay(array1, array2) {
+  let array = _.zip(array1, array2);
+  return array;
+}
+
+const array1 = [1, 2, 3, 4];
+const array2 = ["a", "b", "c", "d"];
+console.log(zipList(array1, array2));
+console.log(zipListTheSimpleWay(array1, array2));
